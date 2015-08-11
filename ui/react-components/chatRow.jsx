ca19a2e9f1664c301417087e319f7c3ec4dcc072
vitalii <borodii@pmscloud.com>
2015-08-11 11:02:37 +0300

    added styles for chat

--- ui/react-components/chatRow.jsx
+++ ui/react-components/chatRow.jsx
@@ -1,5 +1,20 @@
 'use strict'
 var React = require('react');
+var pre = {
+    1: 'one',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen'
+};
 module.exports = React.createClass({
     edit: function () {
         if (this.props.item.done) return;
@@ -13,22 +28,57 @@ module.exports = React.createClass({
     render: function () {
         var item = this.props.item,
             message = item.message,
-            name = item.userName,
-            date = (function (d) {
+            name;
+        var today = new Date(),
+            tYear = today.getFullYear(),
+            tMonth = today.getMonth(),
+            tDay = today.getDate();
+        var date = (function (d) {
                 var date = new Date(d || null),
                     year = date.getFullYear(),
-                    month = date.getMonth(),
-                    day = date.getDate(),
-                    hours = date.getHours(),
-                    minutes = date.getMinutes(),
-                    seconds = date.getSeconds();
-                return /*[day,month,year].join('/') + ' ' +*/ [hours, minutes, seconds].join(':')
+                    month = date.getMonth();
+            if (tYear === year) {
+                if (tMonth === month) {
+                    var day = date.getDate(),
+                        hours = date.getHours(),
+                        minutes = date.getMinutes(),
+                        seconds = date.getSeconds(),
+                        time = [hours, minutes, seconds].join(':');
+                    minutes = minutes.toString().length < 2 ? '0' + minutes : minutes;
+                    seconds = seconds.toString().length < 2 ? '0' + seconds : seconds;
+                    if (tDay === day) {
+                        date = 'Today at ' + time;
+                    } else if (tDay - 1 === day) {
+                        date = 'Yesterday at ' + time;
+                    } else if (yDay > day) {
+                        date = pre[day - yDay] + ' days ago at ' + time;
+                    }
+                } else if (tMonth > month) {
+                    date = pre[tMonth - month] + ' months ago';
+                }
+            } else {
+                date = pre[tYear - year] + ' years ago';
+            }
+            return date
             })(item.createdDate);
-        return (<li className="chatMessage">
-            <div className="info">
-                <p><b>{name}</b> {' wrote:'}<br /> {'at '} {date}</p>
-            </div>
-            <div className="arrow_box">
+        var className,
+            rowClass,
+            mine = item.userId === this.props.userId;
+        if (mine) {
+            rowClass = ''//'chatRight';
+            className = 'bubble bubble-alt';
+            name = 'You'
+        } else {
+            rowClass = ''//'chatLeft';
+            className = 'bubble';
+            name = item.userName
+        }
+        if (item.new) {
+            className += ' appearance';
+        }
+        return (<li className={rowClass}>
+            <div className={className}>
+                <span className="info"><b>{name}</b> {date}</span>
                 <p>{message}</p>
             </div>
         </li>)
