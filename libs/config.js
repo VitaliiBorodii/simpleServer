8e5d5c7859ce053ba8d22d11a8b27e0d1ca067e8
vitalii <borodii@pmscloud.com>
2015-08-25 14:57:35 +0300

    add setiings for openshift redhead

--- libs/config.js
+++ libs/config.js
@@ -1,6 +1,27 @@
 var nconf = require('nconf');
 nconf.argv()
     .env()
-    .file({ file: './config/main.json' });
-
+    .file({file: './config/config.js'});
+nconf.defaults({
+    session: {
+        secret: "random Secret",
+        key: "user",
+        cookie: {
+            maxAge: 604800000,
+            httpOnly: true
+        }
+    }
+});
+var env = process.env;
+if (!nconf.get('port')) {
+    nconf.set('port', env.OPENSHIFT_NODEJS_PORT || 8080)
+}
+if (!nconf.get('mongo')) {
+    nconf.set('mongo', {
+        host: env.OPENSHIFT_MONGODB_DB_HOST,
+        user: env.OPENSHIFT_MONGODB_DB_USERNAME,
+        port: env.OPENSHIFT_MONGODB_DB_PORT,
+        uri: env.OPENSHIFT_MONGODB_DB_URL
+    });
+}
 module.exports = nconf;
